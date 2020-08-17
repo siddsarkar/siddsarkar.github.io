82e78d4e6e840ae18d3e22c4f19b0c41c2740ee2
Siddhartha Sarkar <siddsarkar7@gmail.com>
2020-08-17 14:40:41 +0530

    js animations

--- js/index.js
+++ js/index.js
@@ -1,6 +1,21 @@
-window.addEventListener("DOMContentLoaded", () => {
-  setTimeout(() => {
-    const ele = document.getElementById("root");
-    ele.classList.remove("loading-screen");
-  }, 500);
-});
+const avatar = document.querySelector(".profile-avatar");
+const line = document.querySelector("#line-load");
+const icons = document.getElementsByClassName("fa");
+const name = document.getElementsByClassName("name");
+
+document.onreadystatechange = function () {
+  if (document.readyState == "complete") {
+    setTimeout(() => {
+      const ele = document.getElementById("root");
+      ele.classList.remove("loading-screen");
+      line.classList.add("line");
+      avatar.setAttribute("style", "animation: apearcer 1s ease-in-out");
+      for (let i = 0; i < icons.length; i++) {
+        icons[i].setAttribute("style", "animation: apearcer2 1s ease-in-out");
+      }
+      for (let j = 0; j < name.length; j++) {
+        name[i].setAttribute("style", "animation: apearcer3 1s ease-in-out");
+      }
+    }, 1000);
+  }
+};
