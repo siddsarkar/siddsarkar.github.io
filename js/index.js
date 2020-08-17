e4589e8288e324ccbd058b489767d20c08cd1151
Siddhartha Sarkar <siddsarkar7@gmail.com>
2020-08-17 16:02:27 +0530

    added dark theme

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 const avatar = document.querySelector(".profile-avatar");
+const pic = document.querySelector("#avatar");
 const line = document.querySelector("#line-load");
 const icons = document.getElementsByClassName("fa");
 const name = document.getElementsByClassName("name");
-
 document.onreadystatechange = function () {
+  // document.classList.add("darktheme");
   if (document.readyState == "complete") {
     setTimeout(() => {
       const ele = document.getElementById("root");
@@ -13,9 +14,14 @@ document.onreadystatechange = function () {
       for (let i = 0; i < icons.length; i++) {
         icons[i].setAttribute("style", "animation: apearcer2 1s ease-in-out");
       }
+
       for (let j = 0; j < name.length; j++) {
-        name[i].setAttribute("style", "animation: apearcer3 1s ease-in-out");
+        name[j].classList.add("text-fade");
       }
-    }, 1000);
+    }, 2000);
   }
+};
+
+pic.onclick = () => {
+  document.body.classList.toggle("darktheme");
 };
